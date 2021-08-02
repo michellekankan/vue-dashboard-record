4ae156f80c41ed0a9e610af3c28139ad27d6dcc8
michelle <michellekan0102@gmail.com>
2021-08-02 21:03:05 +0800

    update project

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  created(){
+    const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products`
+    console.log(process.env.APIPATH, process.env.CUSTOMPATH)
+    // API 伺服器路徑
+    // 所申請的 APIPath
+
+    this.$http.get(api).then((response) => {
+      console.log(response.data)
+    })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
